2d771a403cb47093c4d0ab1facb17c21d7d05173
DEATHB4DEFEAT <zachcaffee@outlook.com>
2022-01-23 17:17:33 -0800

    a

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-docker stop simplemodbot-api -t 0
-echo Stopped SimpleModBot-api. | lolcat
-docker build -t simplemodbot-api .
-echo Build complete. | lolcat
-docker-compose up --remove-orphans
